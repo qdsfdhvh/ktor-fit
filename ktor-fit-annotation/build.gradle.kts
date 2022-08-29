ff466639fdf8ae07b3c6f6d8eb467c8c87c83fe4
seiko <seiko_des@outlook.com>
2022-08-29 12:21:07 +0800

    add more platform

--- ktor-fit-annotation/build.gradle.kts
+++ ktor-fit-annotation/build.gradle.kts
@@ -9,31 +9,23 @@ plugins {
 }
 
 kotlin {
+    android {
+        publishLibraryVariants("debug", "release")
+    }
     jvm()
-    linuxX64()
-    macosX64()
-    watchosArm64()
-    watchosX64()
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
     ios()
+    iosSimulatorArm64()
+    tvos()
+    macosX64()
+    macosArm64()
+    linuxX64()
+    linuxArm64()
+    mingwX64()
+    watchosX64()
+    watchosArm64()
     js {
         browser()
         nodejs()
-    }
-    sourceSets {
-        val commonMain by getting
-        val jvmMain by getting
-        val linuxX64Main by getting
-        val macosX64Main by getting
-        val watchosArm64Main by getting
-        val watchosX64Main by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by getting
-        val jsMain by getting
     }
 }
 
