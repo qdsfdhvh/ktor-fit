33dc9bc48c471b07131816a024390b3c0041df53
seiko <seiko_des@outlook.com>
2024-01-15 11:54:41 +0800

    remove auto-service

--- ktor-fit-kcp/build.gradle.kts
+++ ktor-fit-kcp/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   alias(libs.plugins.kotlin.jvm)
-  alias(libs.plugins.ksp)
   alias(libs.plugins.maven.publish)
   alias(libs.plugins.dokka)
 }
@@ -11,9 +10,6 @@ kotlin {
 
 dependencies {
   compileOnly(libs.kotlin.compiler.embeddable)
-
-  compileOnly(libs.auto.service.annotations)
-  ksp(libs.auto.service.ksp)
 
   testImplementation(kotlin("test"))
   testImplementation(libs.kotlin.compile.testing)
