37f0a59c1f7bd3acd64c30d63fba589b8e24db5f
seiko <seiko_des@outlook.com>
2024-02-01 11:29:05 +0800

    update idea plugin sign config

--- ktor-fit-idea-plugin/build.gradle.kts
+++ ktor-fit-idea-plugin/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
   alias(libs.plugins.kotlin.jvm)
   alias(libs.plugins.intelliJPlugin)
@@ -10,6 +12,10 @@ repositories {
   mavenCentral()
 }
 
+kotlin {
+  jvmToolchain(17)
+}
+
 dependencies {
   implementation(projects.ktorFitKcp)
 }
@@ -17,38 +23,38 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.2.5")
-  type.set("IC") // Target IDE Platform
-
-  plugins.set(listOf("Kotlin"))
-}
-
-kotlin {
-  jvmToolchain(17)
+  version = "2023.2.5"
+  plugins = listOf("Kotlin")
+  updateSinceUntilBuild = false
 }
 
 tasks {
-  // // Set the JVM compatibility versions
-  // withType<JavaCompile> {
-  //   sourceCompatibility = "17"
-  //   targetCompatibility = "17"
-  // }
-  // withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-  //   kotlinOptions.jvmTarget = "17"
-  // }
-
   patchPluginXml {
+    version.set("${project.version}")
     sinceBuild.set("222")
     untilBuild.set("233.*")
   }
 
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+  buildPlugin {
+    archiveFileName = "${project.name}-${project.version}.zip"
   }
 
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
+  val file = project.file("signing.properties")
+  if (file.exists()) {
+    val signingProp = Properties()
+    signingProp.load(file.inputStream())
+
+    signPlugin {
+      certificateChainFile.set(file(signingProp.getProperty("CERTIFICATE_CHAIN")))
+      privateKeyFile.set(file(signingProp.getProperty("PRIVATE_KEY")))
+      password.set(signingProp.getProperty("PRIVATE_KEY_PASSWORD"))
+      inputArchiveFile.set(layout.buildDirectory.file("distributions/${project.name}-${project.version}.zip").get().asFile)
+      outputArchiveFile.set(layout.buildDirectory.file("distributions/${project.name}-${project.version}-signed.zip").get().asFile)
+    }
+
+    publishPlugin {
+      distributionFile.set(layout.buildDirectory.file("distributions/${project.name}-${project.version}-signed.zip").get().asFile)
+      token.set(signingProp.getProperty("INTELLIJ_TOKEN"))
+    }
   }
 }
